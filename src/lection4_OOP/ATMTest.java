60bde8a30cc5198c3abf35bc6ff137d2842502a8
Андрей <bazuka271@mail.ru>
2019-07-16 16:32:54 +0300

    TimeLength and ATM from lection4_OOP has been added

---
+++ src/lection4_OOP/ATMTest.java
@@ -0,0 +1,13 @@
+package lection4_OOP;
+
+public class ATMTest {
+    public static void main(String[] args) {
+        ATM atm = new ATM(10, 10, 10);
+        atm.getCache(110);
+        atm.getCache(20);
+        atm.getCache(50);
+        atm.getCache(70);
+        atm.getCache(230);
+        atm.getCache(1000);
+    }
+}
